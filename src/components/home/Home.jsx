affab08da01bb6e4325ea517cee0d7bb845a6e43
luisvill02 <lvillamil@unimagdalena.edu.co>
2023-11-13 23:36:54 -0500

    Add user session functionality to Home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,15 @@
 import { Button, ButtonNavigate } from '../buttons/Button.jsx';
+import { useState } from 'react';
 import "./HomeStyles.css";
 import copa from "../../assets/copaFondo.jpg";
 
 const Home = () => {
+    const [isSesion, setIsSesion] = useState(false);
+    const user = window.localStorage.getItem('user');
+
+    const hanleSesion = (state) => {
+        setIsSesion(state);
+    }
     return (
         <>
             <main>
@@ -10,8 +17,10 @@ const Home = () => {
                     <h1 id='titulo'>Eliminatorias<br/> mundiales de la FIFA</h1>
                     <p id='parrafo'>Consulta el estado de las<br/> eliminatorias rumbo al mundial <br/>de Estados unidos, Cánada y <br/>mexico del 2026.</p>
                     <div className="butons">
-                        <ButtonNavigate text="Login" route="/login" className="botones"/>
-                        <ButtonNavigate text="Register" route="/register" className="botones"/>
+                        {!user ? <Button text="Cerrar sesion" className="botones" onClick={hanleSesion}/> : <>
+                            <ButtonNavigate text="Login" route="/login" className="botones"/>
+                            <ButtonNavigate text="Register" route="/register" className="botones"/>
+                        </>}
                     </div>
                 </section>
                 <section className='image-home'>
